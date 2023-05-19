d65a45e8e4005e8368ae1e0aa26ff0f292d180f7
Sofia Poludina <poludsof@fel.cvut.cz>
2023-05-19 05:12:20 +0200

    Added comments for tests CollisionChecker class and GameObjects class.

--- rpg_game/src/test/java/cz/cvut/fel/pjv/main/CollisionCheckerTest.java
+++ rpg_game/src/test/java/cz/cvut/fel/pjv/main/CollisionCheckerTest.java
@@ -1,111 +1,95 @@
 package cz.cvut.fel.pjv.main;
 
-import cz.cvut.fel.pjv.entity.Entity;
 import cz.cvut.fel.pjv.enemy.Boss;
-import cz.cvut.fel.pjv.enemy.Pirate;
 import cz.cvut.fel.pjv.object.House;
 import junit.framework.TestCase;
 
-import java.awt.*;
-
+/**
+ Test for the CollisionChecker class.
+ */
 public class CollisionCheckerTest extends TestCase {
     GamePanel gp = new GamePanel();
+    String filename = "src/dataJson/new_game.json";
 
-    public void testCheckCollisionEntity() {
-        gp.player.solidArea = new Rectangle(20, 24, 32, 32); // Set up player's solid area
-        gp.player.collision = false;
-        gp.player.worldX = 0;
-        gp.player.worldY = 0;
-
-        Entity[] entities = new Entity[2]; // Create an array of entities
-        entities[0] = new Pirate(gp);
-        entities[0].solidArea = new Rectangle(22, 20, 32, 32); // Set up first entity's solid area
-        entities[0].worldX = 0;
-        entities[0].worldY = 0;
-
-        entities[1] = new Pirate(gp);
-        entities[1].solidArea = new Rectangle(150, 150, 0, 0); // Set up second entity's solid area
-        entities[1].worldX = 0;
-        entities[1].worldY = 0;
-
-        int result = gp.checker.CheckCollisionEntity(gp.player); // Call the method being tested
-        assertTrue(gp.player.collision); // Check if player's collision flag is set
-        assertEquals(0, result); // Check if the method returned the correct index of the colliding entity
-    }
-
+    /**
+     Test to check collision between the player and the boat.
+     */
     public void testCheckCollisionBoat() {
-        gp.player.solidArea = new Rectangle(20, 24, 32, 32);
-
+        gp.player.setDefaultValues(filename);
         gp.player.worldX = 0;  // set the position of the player on the map
         gp.player.worldY = 0;
 
         gp.ship = new Ship(gp);  // create a new boat for testing
-        gp.ship.worldX = 0;  // set the position of the boat on the map
-        gp.ship.worldY = 0;
 
         boolean result;
+
         // Case 1: The player is not colliding with the boat
-        gp.ship.solidArea = new Rectangle(100, 100, 32, 32);
+        gp.ship.worldX = 100;  // set the position of the boat on the map
+        gp.ship.worldY = 100;
         result = gp.checker.CheckCollisionShip(gp.player);
         assertFalse("The player should not be colliding with the boat", result);
 
         // Case 2: The player is colliding with the boat
-        gp.ship.solidArea = new Rectangle(0, 0, 32, 32);
+        gp.ship.worldX = -100;
+        gp.ship.worldY = 0;
         result = gp.checker.CheckCollisionShip(gp.player);
         assertTrue("The player should be colliding with the boat", result);
     }
 
+    /**
+     Test to check collision between the player and the house.
+     */
     public void testCheckCollisionHouse() {
+        gp.player.setDefaultValues(filename);
         gp.player.collision = false;
-        gp.player.solidArea = new Rectangle(20, 24, 32, 32); // Set up player's solid area
         gp.player.worldX = 0;
         gp.player.worldY = 0;
 
+        // Case 1: The player is colliding with the house
         House house1 = new House(gp);
-        house1.solidArea = new Rectangle(32, 32, 64, 64); // Set up house's solid area
-        house1.worldX = 0;
+        house1.worldX = -50;
         house1.worldY = 0;
         gp.house = house1;
 
         gp.checker.CheckCollisionHouse(gp.player); // Call the method being tested
-        assertTrue(gp.player.collision);
+        assertTrue("The player should be colliding with the house", gp.player.collision);
 
+        // Case 2: The player is not colliding with the house
         gp.player.collision = false;
         House house2 = new House(gp);
-        house2.solidArea = new Rectangle(132, 132, 64, 64); // Set up house's solid area
-        house2.worldX = 0;
-        house2.worldY = 0;
+        house2.worldX = 100;
+        house2.worldY = 100;
         gp.house = house2;
 
         gp.checker.CheckCollisionHouse(gp.player); // Call the method being tested
-        assertFalse(gp.player.collision);
+        assertFalse("The player should not be colliding with the house", gp.player.collision);
     }
 
+    /**
+     Test case to check collision between the player and the boss.
+     */
     public void testCheckCollisionBoss() {
-        gp.player.solidArea = new Rectangle(0, 0, 20, 20);
+        gp.player.setDefaultValues(filename);
         gp.player.worldX = 0;
         gp.player.worldY = 0;
-        gp.player.collision = false;
 
         gp.boss = new Boss(gp);
-        gp.boss.solidArea = new Rectangle(0, 8, 32, 32);
         gp.boss.worldX = 0;
         gp.boss.worldY = 0;
 
-        // Вызываем метод CheckCollisionBoss
+        // Case 1: The player is colliding with the boss
         boolean result = gp.checker.CheckCollisionBoss(gp.player);
         assertTrue(result);
-        assertTrue(gp.player.collision);
+        assertTrue("The player should be colliding with the boss", gp.player.collision);
 
-
-        gp.boss.solidArea = new Rectangle(60, 68, 32, 32);
-        gp.boss.worldX = 0;
-        gp.boss.worldY = 0;
+        // Case 2: The player is not colliding with the boss
+        gp.boss.worldX = 100;
+        gp.boss.worldY = 100;
         gp.player.collision = false;
 
         result = gp.checker.CheckCollisionBoss(gp.player);
         assertFalse(result);
-        assertFalse(gp.player.collision);
+        assertFalse("The player should not be colliding with the boss", gp.player.collision);
 
     }
 }
--- rpg_game/src/test/java/cz/cvut/fel/pjv/object/GameObjectsTest.java
+++ rpg_game/src/test/java/cz/cvut/fel/pjv/object/GameObjectsTest.java
@@ -3,44 +3,44 @@ package cz.cvut.fel.pjv.object;
 import cz.cvut.fel.pjv.main.GamePanel;
 import junit.framework.TestCase;
 
+/**
+ Test for the GameObjects class.
+ */
 public class GameObjectsTest extends TestCase {
+    GamePanel gp = new GamePanel();
 
+    /**
+     Test to check picking up objects by the player.
+     */
     public void testPickUpObj() {
-
-        GamePanel gp = new GamePanel();
-        GameObjects obj = new GameObjects();
-
-        // Проверяем, что метод корректно обрабатывает объекты типа "Key"
         gp.objArray[0] = new Key();
         gp.objArray[1] = new Rum();
         gp.objArray[2] = new Sword();
         gp.objArray[3] = new Map();
         gp.objArray[4] = new Heart();
 
-        int Level = gp.player.level;
         int KeyCount = gp.player.key_count;
-        obj.pickUpObj(0, gp);
-        assertEquals(Level + 1, gp.player.level); // Проверяем, что уровень увеличился на 1
-        assertEquals(KeyCount + 1, gp.player.key_count); // Проверяем, что количество ключей уменьшилось на 1
+        GameObjects.pickUpObj(0, gp);
+        assertEquals(KeyCount + 1, gp.player.key_count);
         assertNotNull("The method does not fill the player's inventory.", gp.player.inventory);
 
         int RumCount = gp.player.rum_count;
-        obj.pickUpObj(1, gp);
+        GameObjects.pickUpObj(1, gp);
         assertEquals(RumCount + 1, gp.player.rum_count);
         assertNull("The method does not clear the array after picking up the item.", gp.objArray[1]);
 
         int SwordCount = gp.player.sword_count;
-        obj.pickUpObj(2, gp);
+        GameObjects.pickUpObj(2, gp);
         assertEquals(SwordCount + 1, gp.player.sword_count);
         assertNull("The method does not clear the array after picking up the item.", gp.objArray[2]);
 
         int MapCount = gp.player.map_count;
-        obj.pickUpObj(3, gp);
+        GameObjects.pickUpObj(3, gp);
         assertEquals(MapCount + 1, gp.player.map_count);
         assertNull("The method does not clear the array after picking up the item.", gp.objArray[3]);
 
         int HeartCount = gp.player.heart_count;
-        obj.pickUpObj(4, gp);
+        GameObjects.pickUpObj(4, gp);
         if (gp.player.heart_count < gp.player.max_health)
             assertEquals(HeartCount + 1, gp.player.heart_count);
         else
@@ -48,9 +48,11 @@ public class GameObjectsTest extends TestCase {
         assertNull("The method does not clear the array after picking up the item.", gp.objArray[4]);
     }
 
+    /**
+     Test to check picking up all types of doors by the player.
+     */
     public void testAllDoors() {
         GamePanel gp = new GamePanel();
-        GameObjects obj = new GameObjects();
 
         for (int i = 0; i < 6; ++i) {
             gp.objArray[0] = new RDoor();
@@ -60,8 +62,8 @@ public class GameObjectsTest extends TestCase {
             gp.objArray[4] = new HellRDoor();
             gp.objArray[5] = new HellLDoor();
             gp.player.key_count = 1;
-            obj.pickUpObj(i, gp);
-            assertEquals(0, gp.player.key_count); // Проверяем, что количество ключей уменьшилось на 1
+            GameObjects.pickUpObj(i, gp);
+            assertEquals(0, gp.player.key_count);
             assertNull("The method does not clear the array after picking up the item.", gp.objArray[i]);
         }
 
@@ -69,8 +71,8 @@ public class GameObjectsTest extends TestCase {
         gp.objArray[5] = new HellLDoor();
         gp.player.key_count = 0;
         for (int i = 0; i < 6; ++i) {
-            obj.pickUpObj(i, gp);
-            assertEquals(0, gp.player.key_count); // Проверяем, что количество ключей не уменьшилось
+            GameObjects.pickUpObj(i, gp);
+            assertEquals(0, gp.player.key_count);
             assertNotNull("The door is removed from the array when the player has no key.", gp.objArray[i]);
         }
     }
