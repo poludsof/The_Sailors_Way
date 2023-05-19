182cf41b8f5949b343b46d28a1410c420b311c0d
Sofia Poludina <poludsof@fel.cvut.cz>
2023-05-19 04:43:09 +0200

    Added comments for tests Boss class and Player class.

--- rpg_game/src/test/java/cz/cvut/fel/pjv/enemy/BossTest.java
+++ rpg_game/src/test/java/cz/cvut/fel/pjv/enemy/BossTest.java
@@ -3,40 +3,53 @@ package cz.cvut.fel.pjv.enemy;
 import cz.cvut.fel.pjv.main.GamePanel;
 import junit.framework.TestCase;
 
+/**
+ Test for the Boss class.
+ */
 public class BossTest extends TestCase {
     GamePanel gp = new GamePanel();
+    String filename = "rpg_game/src/dataJson/test_boss.json";
 
+    /**
+     Verifies if the default values are set correctly.
+     */
     public void testSetDefaultValues() {
-        gp.boss.setDefaultValues("rpg_game/src/dataJson/test_boss.json");
-        assertEquals(666 * gp.tileSize, gp.boss.worldX); // Проверяем, что количество ключей не уменьшилось
-        assertEquals(4 * gp.tileSize, gp.boss.worldY); // Проверяем, что количество ключей не уменьшилось
-        assertEquals(1, gp.boss.speed); // Проверяем, что количество ключей не уменьшилось
-        assertEquals(0, gp.boss.heart_count); // Проверяем, что количество ключей не уменьшилось
+        gp.boss.setDefaultValues(filename);
+        assertEquals(666 * gp.tileSize, gp.boss.worldX);
+        assertEquals(4 * gp.tileSize, gp.boss.worldY);
+        assertEquals(1, gp.boss.speed);
+        assertEquals(0, gp.boss.heart_count);
     }
 
+    /**
+     Checks the change of direction of the boss.
+     */
     public void testRandomBossAction() {
+        // Until the number of frames reaches a certain value, the boss moves in one direction.
         String past_direction = gp.boss.direction;
         gp.boss.frames = 0;
         gp.boss.randomBossAction();
         assertEquals(1, gp.boss.frames);
-        assertEquals(past_direction, gp.boss.direction); // никак не изменилось
+        assertEquals(past_direction, gp.boss.direction);
 
+        // The direction has changed.
         gp.boss.frames = 80;
+        past_direction = gp.boss.direction;
         gp.boss.randomBossAction();
-        assertEquals(0, gp.boss.frames);
+        assertEquals(1, gp.boss.frames);
+        assertNotSame(past_direction, gp.boss.direction);
     }
 
+    /**
+     Verifies the behavior when the boss is hit by the player's attack.
+     */
     public void testFightBoss() {
-        // не заходит в иф
+        // If the boss is in a state after the hit, he cannot take the hit again.
         gp.boss.timeToDamage = true;
         gp.boss.fightBoss();
         assertTrue(gp.boss.timeToDamage);
 
-        gp.player.attacking = true;
-        gp.boss.fightBoss();
-        assertTrue(gp.boss.timeToDamage);
-
-        //in if
+        // The player attacks the boss.
         gp.boss.timeToDamage = false;
         gp.player.attacking = true;
         gp.boss.heart_count = 5;
@@ -44,11 +57,11 @@ public class BossTest extends TestCase {
         assertEquals(4, gp.boss.heart_count);
         assertTrue(gp.boss.showHealth);
 
+        // The boss is dead and must disappear.
         gp.boss.timeToDamage = false;
         gp.player.attacking = true;
         gp.boss.heart_count = 0;
         gp.boss.fightBoss();
-        System.out.println(gp.boss);
-        assertNull("The method does not remove the boss when it runs out of lives", gp.boss);
+        assertNull("The method does not remove the boss when it runs out of lives.", gp.boss);
     }
 }
--- rpg_game/src/test/java/cz/cvut/fel/pjv/entity/PlayerTest.java
+++ rpg_game/src/test/java/cz/cvut/fel/pjv/entity/PlayerTest.java
@@ -4,80 +4,66 @@ import cz.cvut.fel.pjv.main.GamePanel;
 import cz.cvut.fel.pjv.main.State;
 import junit.framework.TestCase;
 
+/**
+ Test for the Player class.
+ */
 public class PlayerTest extends TestCase {
     GamePanel gp = new GamePanel();
+    String filename = "src/dataJson/new_game.json";
+
+    /**
+     Verifies if the default values are set correctly.
+     */
     public void testSetDefaultValues() {
-        gp.player.setDefaultValues("rpg_game/src/dataJson/test_player.json");
-        assertEquals(999 * gp.tileSize, gp.player.worldX);
-        assertEquals(666 * gp.tileSize, gp.player.worldY);
+        gp.player.setDefaultValues(filename);
+        assertEquals(10 * gp.tileSize, gp.player.worldX);
+        assertEquals(93 * gp.tileSize, gp.player.worldY);
         assertEquals(6, gp.player.speed);
         assertEquals(6, gp.player.heart_count);
-        assertEquals(2, gp.player.key_count);
-        assertEquals(3, gp.player.rum_count);
-        assertEquals(4, gp.player.map_count);
-        assertEquals(5, gp.player.sword_count);
-        assertEquals(10, gp.player.level);
-        assertEquals(9, gp.player.dead_pirate_count);
+        assertEquals(0, gp.player.key_count);
+        assertEquals(0, gp.player.rum_count);
+        assertEquals(0, gp.player.map_count);
+        assertEquals(0, gp.player.sword_count);
+        assertEquals(1, gp.player.level);
+        assertEquals(0, gp.player.dead_pirate_count);
     }
 
+    /**
+     Verifies if the inventory is filled correctly based on the player's counts.
+     */
     public void testFillInventory() {
+
         gp.player.map_count = 1;
         gp.player.rum_count = 2;
         gp.player.key_count = 3;
         gp.player.sword_count = 4;
+
         gp.player.fillInventory();
+
         assertNotNull(gp.player.inventory);
+
         assertEquals(1, (int) gp.player.inventory.get("Map"));
         assertEquals(2, (int) gp.player.inventory.get("Rum"));
         assertEquals(3, (int) gp.player.inventory.get("Key"));
         assertEquals(4, (int) gp.player.inventory.get("Sword"));
     }
 
+    /**
+     Verifies the behavior of the player's update based on different options.
+     */
     public void testUpdate() {
-        gp.player.heart_count = 0; // check end of game state
-        gp.player.update();
-        assertEquals(State.GAME_OVER, gp.state);
 
-        gp.player.timeToDamage = true; // check damage timer
-        gp.player.damageCounter = 59;
-        gp.player.update();
-        assertFalse(gp.player.timeToDamage);
+        checkEndOfGameState();
 
-        gp.player.keyH.spacePressed = true; // check attack after press space
-        gp.player.update();
-        assertTrue(gp.player.attacking);
+        checkAttackState();
+        checkAttackArea();
 
-        gp.player.keyH.swordButton = true; // check the changes of the attacking area when the Sword is applied
-        gp.player.attacking = false;
-        gp.player.sword_count = 1;
-        gp.player.update();
-        assertEquals(60, gp.player.attackArea.width);
-        assertEquals(60, gp.player.attackArea.height);
-
-        gp.player.rum_time_start = false; // check if the data has not changed when the rum_key_button is not pressed
-        int past_speed = gp.player.speed;
-        assertEquals(past_speed, gp.player.speed);
-
-        gp.player.keyH.upPressed = true;
-        gp.player.attacking = false;
-        gp.player.keyH.rumButton = true;
-        gp.player.rum_count = 1;
-        past_speed = gp.player.speed;
-        gp.player.update();
-        assertTrue(gp.player.rum_time_start);
-        assertEquals(past_speed + 4, gp.player.speed);
-        assertEquals(0, gp.player.rum_count);
-
-        gp.player.keyH.upPressed = true;
-        gp.player.attacking = false;
-        gp.player.rum_time_start = true;
-        gp.player.rum_time = 5 * 60 + 1;
-        past_speed = gp.player.speed;
-        gp.player.update();
-        assertFalse(gp.player.rum_time_start);
-        assertEquals(past_speed - 4, gp.player.speed);
+        checkRumSpeed();
     }
 
+    /**
+     Check the player's movement on the map.
+     */
     public void testDoMove() {
         int pastCoord = gp.player.worldX;
         gp.player.direction = "left";
@@ -94,9 +80,71 @@ public class PlayerTest extends TestCase {
         gp.player.doMove();
         assertEquals(pastCoord - gp.player.speed, gp.player.worldY);
 
-         pastCoord = gp.player.worldY;
-         gp.player.direction = "down";
-         gp.player.doMove();
-         assertEquals(pastCoord + gp.player.speed, gp.player.worldY);
+        pastCoord = gp.player.worldY;
+        gp.player.direction = "down";
+        gp.player.doMove();
+        assertEquals(pastCoord + gp.player.speed, gp.player.worldY);
+    }
+
+    /**
+     Checks the game's end state when the player has no life left.
+     */
+    private void checkEndOfGameState() {
+        gp.player.setDefaultValues(filename);
+
+        gp.player.heart_count = 0;
+        gp.player.update();
+        assertEquals(State.GAME_OVER, gp.state);
+    }
+
+    /**
+     Checks attack state after press space.
+     */
+    private void checkAttackState() {
+        gp.player.setDefaultValues(filename);
+
+        gp.player.keyH.spacePressed = true; // check attack after press space
+        gp.player.update();
+        assertTrue(gp.player.attacking);
+    }
+
+    /**
+     Checks the changes of the attacking area when the Sword is applied.
+     */
+    private void checkAttackArea() {
+        gp.player.setDefaultValues(filename);
+
+        gp.player.keyH.swordButton = true;
+        gp.player.attacking = false;
+        gp.player.sword_count = 1;
+        gp.player.update();
+        assertEquals(60, gp.player.attackArea.width);
+        assertEquals(60, gp.player.attackArea.height);
+    }
+
+    /**
+     Check if the data has not changed when the rum_key_button is not pressed/pressed.
+     */
+    private void checkRumSpeed() {
+        gp.player.setDefaultValues(filename);
+
+        // Check speed increase when rum_key_button is pressed.
+        gp.player.attacking = false;
+        gp.player.keyH.rumButton = true;
+        gp.player.rum_count = 1;
+        int past_speed = gp.player.speed;
+        gp.player.update();
+        assertTrue(gp.player.rum_time_start);
+        assertEquals(past_speed + 4, gp.player.speed);
+        assertEquals(0, gp.player.rum_count);
+
+        gp.player.attacking = false;
+        gp.player.rum_time_start = true;
+        gp.player.rum_time = 5 * 60 + 1;
+        past_speed = gp.player.speed;
+        gp.player.update();
+        assertFalse(gp.player.rum_time_start);
+        assertEquals(past_speed - 4, gp.player.speed);
+
     }
 }
